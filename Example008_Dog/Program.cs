3fa3c217d523caa66a5969e128c84f9b6591c5b6
Pavel Pirogov <televizor50@yandex.ru>
2023-01-27 22:59:14 +0300

    add Example 008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = new Random().Next(1000, 1000001);
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+System.Console.WriteLine($"Дистанция между друзьями {distance} метров");
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+System.Console.WriteLine($"Собака пробежала между друзьями {count} раз");
+// System.Console.WriteLine("Собака пробежала между друзьями " + count + " раз");
