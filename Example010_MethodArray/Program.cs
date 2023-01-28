6519bf7a5bc5207d0e9c8c2877bb5729f5943829
Pavel Pirogov <televizor50@yandex.ru>
2023-01-28 14:12:13 +0300

    add Examples009-011 lecture02 Function, Array, Void

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 20, 93, 74, 5, 26, 347, 8, 5, 10, 181, 12, 133, 114, 15 };
+
+int n = array.Length; // Количестро элементов в массиве
+int find = 5;
+Console.WriteLine("Искомое число " + find);
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("Индекс искомого числа " + index);
+        break;
+    }
+    index++;
+}
+//Console.WriteLine("Искомое число в массиве не найдено");
